06d6056df7b0487e266cc0254144839e434e2191
Mahfuzur Rahman <mahfujurrahman291003@gmail.com>
2024-03-03 16:26:33 +0600

    dark mode light mode feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import { React } from 'react';
+import { React, useState, useEffect } from "react";
+
+const getUserTheme = () => {
+  const theme = localStorage.getItem("theme") || "dark";
+  return theme === "dark" ? true : false;
+};
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(getUserTheme());
+
+  useEffect(() => {
+    document.documentElement.className = `${isDarkMode && "dark"}`;
+    localStorage.setItem("theme", isDarkMode ? "dark" : "light");
+  }, [isDarkMode]);
+
   return (
-    <div className="App">
-      
+    <div className="container">
+      <div className="toggle-btn">
+        <input
+          type="checkbox"
+          id="toggle"
+          checked={isDarkMode}
+          onChange={(e) => setIsDarkMode(e.target.checked)}
+        />
+        <label htmlFor="toggle"></label>
+      </div>
     </div>
   );
 }
